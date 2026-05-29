b58cf5419167fcf28ae84c8a23b69b43fa4cd639
Samarth Sugandhi <ignobita143@gmail.com>
2026-05-29 18:14:53 +0530

    feat(auth): add Forgot Password flow with Firebase password reset email

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './services/firebase';
 import { Toaster } from 'react-hot-toast';
 import LandingPage from './components/Landing/LandingPage';
+import ForgotPassword from './components/Auth/ForgotPassword';
 import EditorPage from './components/Editor/EditorPage';
 import VideoCall from './components/Editor/VideoCall';
 import OfflineBanner from './components/Editor/OfflineBanner';
@@ -42,6 +43,7 @@ export default function App() {
       />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/editor" element={<EditorPage user={user} />} />
         {/* Test route to render VideoCall directly for e2e tests */}
         <Route
